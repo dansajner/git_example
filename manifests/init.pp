39c0fbc2c1ce281617d7b5eb9d89f64ed0d044a8
Dan Sajner <dsajner@CMIFMOSXLT011.local>
2015-03-05 21:02:49 -0500

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+class git {
+
+  package { 'git':
+    ensure => installed,
+  }
+
+  $repo = '/home/vagrant/repo'
+
+  file { $repo:
+    ensure => directory,
+    owner  => 'vagrant',
+    group  => 'vagrant',
+  }
+
+  exec { "/usr/bin/git init $repo":
+    creates => "${repo}/.git/config",
+    user    => 'vagrant',
+    group   => 'vagrant',
+    require => [ File[ $repo ], Package[ 'git' ] ],
+  }
+
+  file { "${repo}/.git/hooks/post-commit":
+    ensure  => present,
+    owner   => 'vagrant',
+    group   => 'vagrant',
+    mode    => '0755',
+    content => 'curl http://localhost:8080/job/test/build?delay=0sec',
+    require => Exec[ "/usr/bin/git init $repo" ],
+  }
+
+}
